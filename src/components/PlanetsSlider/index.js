54b12e091c79f52e5c40cddbbe2ce688840e7169
Surya T N <suryatej.nim@yahoo.com>
2022-11-01 06:53:19 +0000

    Implemented JSX element tree in PlanetsSlider component to render the intended UI

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -1,1 +1,29 @@
-// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const sliderProps = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
+  return (
+    <div className="planets-slider-bg-container">
+      <h1 className="planets-slider-header">PLANETS</h1>
+      <Slider {...sliderProps}>
+        {planetsList.map(planetsListItem => (
+          <PlanetItem itemData={planetsListItem} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
