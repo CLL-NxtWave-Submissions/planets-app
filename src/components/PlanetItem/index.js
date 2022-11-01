4d640d4ea61c71911d437ab3aafcf1099b611fa9
Surya T N <suryatej.nim@yahoo.com>
2022-11-01 07:00:09 +0000

    Implemented the JSX element tree in PlanetItem component to render the intended UI

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -1,1 +1,16 @@
-// Write your code here
+import './index.css'
+
+const PlanetItem = props => {
+  const {itemData} = props
+  const {imageUrl, name, description} = itemData
+
+  return (
+    <div className="planet-item-container">
+      <img className="planet-item-img" src={imageUrl} alt={name} />
+      <h1 className="planet-item-name">{name}</h1>
+      <p className="planet-item-description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
